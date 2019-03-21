dfb4d329c37d3d1065a6a95bd3d5d57841f6772c
karinaketc <karinaketc@outlook.com>
2019-03-21 15:15:43 +0200

    создан

---
+++ hex2string/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace hex2string
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.input.Location = new System.Drawing.Point(0, 22);
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.input.Size = new System.Drawing.Size(300, 181);
+            this.input.TabIndex = 1;
+            this.input.Text = "68656c6f20776f726c64";
+            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // output
+            // 
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.output.Location = new System.Drawing.Point(0, 227);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(300, 216);
+            this.output.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "In:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Out:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ hex2string/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hex2string
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            Convert_();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Convert_();
+        }
+
+        void Convert_()
+        {
+            output.Text = "";
+            string str = input.Text.Replace(" ", "");
+
+            try
+            {
+                for (int i = 0; i < str.Length; i += 2)
+                {
+                    string hs = string.Empty;
+
+                    hs = str.Substring(i, 2);
+                    ulong decval = Convert.ToUInt64(hs, 16);
+                    long deccc = Convert.ToInt64(hs, 16);
+                    char character = Convert.ToChar(deccc);
+                    output.Text += character;
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+        }
+    }
+}
